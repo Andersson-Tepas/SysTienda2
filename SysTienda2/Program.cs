007e89da87d797ae464211b31871d61ce6ae117b
Andersson Tepas <anderssontepas@outlook.com>
2025-02-26 10:20:41 -0600

    Agregue

--- SysTienda2/Program.cs
+++ SysTienda2/Program.cs
@@ -1,7 +1,10 @@
+using SysTienda2.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BDContext>();
 
 var app = builder.Build();
 
